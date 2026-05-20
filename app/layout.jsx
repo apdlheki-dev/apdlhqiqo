9d5212e47f8ceeac76b4b2fad80547a09a9137e6
apdlheki-dev <mhsn63264@gmail.com>
2026-05-20 23:52:27 +0300

    Add files via upload

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+
+export const metadata = {
+  title: "People Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ar">
+      <body>{children}</body>
+    </html>
+  );
+}
